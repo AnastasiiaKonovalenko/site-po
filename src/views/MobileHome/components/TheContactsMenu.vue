3456f46ee712f535caa6c2cc98a751f9238c375b
anastasiakonovalenko <konovalenko.a@novaposhta.ua>
2022-09-15 23:53:07 +0300

    first commit

--- src/views/MobileHome/components/TheContactsMenu.vue
+++ src/views/MobileHome/components/TheContactsMenu.vue
@@ -26,7 +26,7 @@
                 target="_blank"
                 href="https://www.linkedin.com/in/olha-/"
             >
-                <BaseIcon :path="mdiLinkedin" type="mdi" />
+                <TheLinkedin class="z-index-1" />
             </a>
         </li>
         <li class="contacts-icon ml-4 rounded-circle">
@@ -44,16 +44,13 @@
 <script setup>
 import TheMail from '../../components/icons/TheMail.vue';
 import TheTelephone from '../../components/icons/TheTelephone.vue';
+import TheLinkedin from '../../components/icons/TheLinkedin.vue';
 import TheWatsapp from '../../components/icons/TheWatsapp.vue';
-import { mdiLinkedin } from '@mdi/js';
-import BaseIcon from '../../components/icons/BaseIcon.vue';
-
-console.log(mdiLinkedin);
 </script>
 
 <style scoped>
 .contacts-icon {
-    background-color: red;
+    background-color: black;
     padding: 8px;
     width: 40px;
     height: 40px;
